a91bb5c631ef6204a3de4432087b619bbf7fc58a
mydu <dumengying1989@gmail.com>
2025-01-16 10:32:53 +0100

    setup

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,19 @@
-<h1 class="font-bold text-xl">
-    Hello world!
-</h1>
+<script>
+    import Scrollytelling from './Scrollytelling.svelte';
+    import intro from './assets/intro.png'
+  </script>
+  
+  <main class="w-full h-screen m-0 relative">
+    <div class="h-screen">
+        <img class="w-full" src={intro} />
+        <div class="mx-auto p-4 w-3/4">
+
+            <h1 class="font-bold text-xl">Unveiling Reciprocal Interactions and Dependencies at the Crossroads of Mass Tourism and the Climate Crisis</h1>
+            <p>
+                This audiovisual project is the result of in-depth research undertaken as part of the Goethe auf Mallorca artistic residency at Casa Planas in Palma de Mallorca, facilitated by the Goethe Institut Barcelona. To foster a more data-driven, explanatory, and exploratory approach, this scrollytelling, as an output of ongoing and iterative research, segments the narrative into distinct sections. Each section integrates data sonification, soundscapes, generative imagery, and archival footage. Notably, all the data trends referenced are reflected through data sonification, while the accompanying visuals partially represent these data points. The visuals also offer a conceptual interpretation of the diffuse economy of tourism, incorporating abstract imagery, architectural generative approaches, and archival footage from the extensive archive of Casa Planas. This segmentation transforms the traditional linear video into an interactive and explorative experience.
+            </p>
+        </div>
+    </div>
+    <!-- <div class="absolute inset-0 bg-gray-800 opacity-80"></div> -->
+    <Scrollytelling />
+  </main>
---
+++ src/Scrollytelling.svelte
@@ -0,0 +1,102 @@
+<script>
+    import { onMount, onDestroy} from 'svelte';
+      
+    const sections = [
+        {
+            title: "Data-Tendency No: 1",
+            subtitle:"Dense",
+            content:'<p>he title "Balearisation" reflects how humans, as a geophysical force, have reshaped Mallorca through mass tourism, transforming it into one of Europe’s densest and most environmentally strained areas. Coined to describe the unchecked expansion of tourism and its adverse consequences, the term has become a widely recognised reference, originating from Mallorca and now used to critique similar phenomena globally. This transformation becomes starkly evident when comparing population densities: Mainland Spain averages 94 people per square kilometre, while Mallorca’s density surges to around 700 during peak summer tourist season.This chapter traces the anthropogenic evolution of Mallorca, highlighting the density and interconnectedness of human impacts. In 63 seconds, it spans from 1842 to today, drawing on census data that grows more detailed over time—from sporadic early records to annual updates by the 21st century. Through sonification of census and visitor data, it captures key anthropogenic events, from the first telegraph route to the carbon footprint of reverse osmosis desalination systems, providing a concise foundation for understanding human influence on the island.</p>'
+        },
+        { 
+            title: "Data-Tendency No: 2",
+            subtitle: "Disproportional",
+            content:'<p>he title "Balearisation" reflects how humans, as a geophysical force, have reshaped Mallorca through mass tourism, transforming it into one of Europe’s densest and most environmentally strained areas. Coined to describe the unchecked expansion of tourism and its adverse consequences, the term has become a widely recognised reference, originating from Mallorca and now used to critique similar phenomena globally. This transformation becomes starkly evident when comparing population densities: Mainland Spain averages 94 people per square kilometre, while Mallorca’s density surges to around 700 during peak summer tourist season.This chapter traces the anthropogenic evolution of Mallorca, highlighting the density and interconnectedness of human impacts. In 63 seconds, it spans from 1842 to today, drawing on census data that grows more detailed over time—from sporadic early records to annual updates by the 21st century. Through sonification of census and visitor data, it captures key anthropogenic events, from the first telegraph route to the carbon footprint of reverse osmosis desalination systems, providing a concise foundation for understanding human influence on the island.</p>'},
+        { 
+            title: "Data-Tendency No: 3",
+            subtitle:"Exponential",
+            content:'<p>he title "Balearisation" reflects how humans, as a geophysical force, have reshaped Mallorca through mass tourism, transforming it into one of Europe’s densest and most environmentally strained areas. Coined to describe the unchecked expansion of tourism and its adverse consequences, the term has become a widely recognised reference, originating from Mallorca and now used to critique similar phenomena globally. This transformation becomes starkly evident when comparing population densities: Mainland Spain averages 94 people per square kilometre, while Mallorca’s density surges to around 700 during peak summer tourist season.This chapter traces the anthropogenic evolution of Mallorca, highlighting the density and interconnectedness of human impacts. In 63 seconds, it spans from 1842 to today, drawing on census data that grows more detailed over time—from sporadic early records to annual updates by the 21st century. Through sonification of census and visitor data, it captures key anthropogenic events, from the first telegraph route to the carbon footprint of reverse osmosis desalination systems, providing a concise foundation for understanding human influence on the island.</p>'},
+        { 
+            title: "Data-Tendency No: 4",
+            subtitle: "Undulating",
+            content:'<p>he title "Balearisation" reflects how humans, as a geophysical force, have reshaped Mallorca through mass tourism, transforming it into one of Europe’s densest and most environmentally strained areas. Coined to describe the unchecked expansion of tourism and its adverse consequences, the term has become a widely recognised reference, originating from Mallorca and now used to critique similar phenomena globally. This transformation becomes starkly evident when comparing population densities: Mainland Spain averages 94 people per square kilometre, while Mallorca’s density surges to around 700 during peak summer tourist season.This chapter traces the anthropogenic evolution of Mallorca, highlighting the density and interconnectedness of human impacts. In 63 seconds, it spans from 1842 to today, drawing on census data that grows more detailed over time—from sporadic early records to annual updates by the 21st century. Through sonification of census and visitor data, it captures key anthropogenic events, from the first telegraph route to the carbon footprint of reverse osmosis desalination systems, providing a concise foundation for understanding human influence on the island.</p>'},
+        { 
+            title: "Data-Tendency No: 5",
+            subtitle: "Dark Figure",
+            content:'<p>he title "Balearisation" reflects how humans, as a geophysical force, have reshaped Mallorca through mass tourism, transforming it into one of Europe’s densest and most environmentally strained areas. Coined to describe the unchecked expansion of tourism and its adverse consequences, the term has become a widely recognised reference, originating from Mallorca and now used to critique similar phenomena globally. This transformation becomes starkly evident when comparing population densities: Mainland Spain averages 94 people per square kilometre, while Mallorca’s density surges to around 700 during peak summer tourist season.This chapter traces the anthropogenic evolution of Mallorca, highlighting the density and interconnectedness of human impacts. In 63 seconds, it spans from 1842 to today, drawing on census data that grows more detailed over time—from sporadic early records to annual updates by the 21st century. Through sonification of census and visitor data, it captures key anthropogenic events, from the first telegraph route to the carbon footprint of reverse osmosis desalination systems, providing a concise foundation for understanding human influence on the island.</p>'},
+        { 
+            title: "Data-Tendency No: 6",
+            subtitle:"Deficient",
+            content:'<p>he title "Balearisation" reflects how humans, as a geophysical force, have reshaped Mallorca through mass tourism, transforming it into one of Europe’s densest and most environmentally strained areas. Coined to describe the unchecked expansion of tourism and its adverse consequences, the term has become a widely recognised reference, originating from Mallorca and now used to critique similar phenomena globally. This transformation becomes starkly evident when comparing population densities: Mainland Spain averages 94 people per square kilometre, while Mallorca’s density surges to around 700 during peak summer tourist season.This chapter traces the anthropogenic evolution of Mallorca, highlighting the density and interconnectedness of human impacts. In 63 seconds, it spans from 1842 to today, drawing on census data that grows more detailed over time—from sporadic early records to annual updates by the 21st century. Through sonification of census and visitor data, it captures key anthropogenic events, from the first telegraph route to the carbon footprint of reverse osmosis desalination systems, providing a concise foundation for understanding human influence on the island.</p>'},
+        { 
+            title: "Data-Tendency No: 7",
+            subtitle:"Increasing",
+            content:'<p>he title "Balearisation" reflects how humans, as a geophysical force, have reshaped Mallorca through mass tourism, transforming it into one of Europe’s densest and most environmentally strained areas. Coined to describe the unchecked expansion of tourism and its adverse consequences, the term has become a widely recognised reference, originating from Mallorca and now used to critique similar phenomena globally. This transformation becomes starkly evident when comparing population densities: Mainland Spain averages 94 people per square kilometre, while Mallorca’s density surges to around 700 during peak summer tourist season.This chapter traces the anthropogenic evolution of Mallorca, highlighting the density and interconnectedness of human impacts. In 63 seconds, it spans from 1842 to today, drawing on census data that grows more detailed over time—from sporadic early records to annual updates by the 21st century. Through sonification of census and visitor data, it captures key anthropogenic events, from the first telegraph route to the carbon footprint of reverse osmosis desalination systems, providing a concise foundation for understanding human influence on the island.</p>'}
+    ];
+  
+    let currentSection = 0;
+  
+  function setupIntersectionObserver() {
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.5
+    };
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          const index = parseInt(entry.target.getAttribute('data-index') || '0');
+          currentSection = index;
+        }
+      });
+    }, options);
+    document.querySelectorAll('.scroll-section').forEach(section => {
+      observer.observe(section);
+    });
+  }
+    let tooltipX=0;
+    let tooltipY=0;
+    let isTooltip=false;
+    let activeTooltip = null;
+    let clientWidth;
+    let clientHeight;
+    
+    const showTooltip = (e,index) => {
+      console.log(e,index)
+      isTooltip=true;
+      tooltipX=e.clientX+200<clientWidth ? e.clientX+20: e.clientX-200; 
+      tooltipY= e.clientY+10<clientHeight ? e.clientY+10 : e.clientY-100;
+      activeTooltip = tooltips[index]
+    }
+    const handleScroll = () => {
+      isTooltip=false;
+      activeTooltip = null;
+    };
+    onMount(() => {
+      setupIntersectionObserver();
+      // window.addEventListener('scroll', handleScroll);
+    });
+    // onDestroy(() => {
+    //   window.removeEventListener('scroll', handleScroll);
+    // });
+  </script>
+    <div class="relative w-full flex bg-[#ccc]">
+        <!-- Sticky sidebar -->
+
+        <div class="w-1/3">
+            {#each sections as section, i}
+            <div class=" z-10 pointer-events-none"> 
+                <div class="scroll-section min-h-screen flex items-center p-8" data-index={i}>
+                    <div class="relative w-full p-8  flex flex-col items-center justify-center gap-4">
+                        <h2>{section.title}</h2>
+                        <h1>{section.subtitle}</h1>
+                        <p>{@html section.content}</p>
+                    </div>
+                </div>
+            </div>
+        {/each}
+        </div>
+
+        <div class="sticky top-0 right-0 h-screen w-2/3 flex items-center justify-center p-4 z-0 bg-black">
+        </div>
+  </div>
+  <svelte:window on:scroll={handleScroll} />
